01fe58dc98c0c51c8871b0c2ccf844031cbe3979
Devvora <devvora08@gmail.com>
2024-12-30 20:31:05 +0530

    final commit for the project

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,8 @@
 import React, { useContext } from 'react'
 import {Route, Routes} from 'react-router-dom'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 import Home from './pages/Home'
 import ApplyJob from './pages/ApplyJob'
 import Applications from './pages/Applications'
@@ -13,19 +16,24 @@ import 'quill/dist/quill.snow.css'
 
 const App = () => {
 
-  const {showRecruiterLogin} = useContext(AppContext)
+  const {showRecruiterLogin, companyToken} = useContext(AppContext)
 
   return (
     <div>
       { showRecruiterLogin && <RecruiterLogin/>}
+      <ToastContainer/>
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/apply-job/:id' element={<ApplyJob/>}/>
         <Route path='/applications' element={<Applications/>}/>
         <Route path='/dashboard' element={<Dashboard/>}>
-          <Route path='add-job' element={<AddJob/>}/>
+          {companyToken ? <>
+            <Route path='add-job' element={<AddJob/>}/>
           <Route path='manage-jobs' element={<ManageJob/>}/>
           <Route path='view-applications' element={<ViewApplications/>}/>
+          </> : null
+          }
+          
         </Route>
       </Routes>
     </div>
--- server/server.js
+++ server/server.js
@@ -1,18 +1,27 @@
 import './config/instrument.js'
 import express from 'express'
 import cors from 'cors'
+import {clerkMiddleware} from '@clerk/express'
 import "dotenv/config"
 import connectDB from './config/db.js'
 
 import * as Sentry from "@sentry/node";
 import { clerkWebhooks } from './controller/webhooks.js'
 
+import companyRoutes from './routes/companyRoutes.js'
+import jobRoutes from './routes/jobRoutes.js'
+import userRoutes from './routes/userRoutes.js'
+
+import connectCloudinary from './config/cloudinary.js'
+
 const app = express()
 
 await connectDB()
+await connectCloudinary()
 
 app.use(cors())
 app.use(express.json())
+app.use(clerkMiddleware())
 
 //Routes
 app.get('/',(req,res)=> {
@@ -22,6 +31,11 @@ app.get("/debug-sentry", function mainHandler(req, res) {
     throw new Error("My first Sentry error!");
 });
 app.post('/webhooks',clerkWebhooks)
+
+
+app.use('/api/company',companyRoutes)
+app.use('/api/jobs',jobRoutes)
+app.use('/api/users',userRoutes)
 
 //PORT
 const PORT = process.env.PORT || 5000
